41139933d8a0b84f0bc395ff371d1c4b06386765
Youssof Fahmy <youssoffahmy@gmail.com>
2021-01-14 00:07:31 -0500

    added react front end

--- web-scraper/react-challenge/src/App.js
+++ web-scraper/react-challenge/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useState} from 'react'
+import {Card, Container, FormControl, Button} from 'react-bootstrap'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	
+	const [url, setUrl] = useState('')
+	
+	function submit(){
+		console.log(url)
+	}
+	
+	return (
+		<div className="App">
+			
+			<Container>
+				<div className="topBar">
+					
+					<FormControl 
+						className='urlBar' 
+						value={url} 
+						onChange={e=>setUrl(e.target.value)}
+					/>
+					
+					<Button  className = 'submitButton' onClick={submit}>Submit</Button>
+				</div>
+				
+				
+				<Card className='mainCard'>
+					<Card.Body>
+						Scraped Content Goes Here!
+					</Card.Body>
+					
+				</Card>
+				
+			</Container>
+			
+		</div>
+	)
 }
 
 export default App;
--- web-scraper/server/main.js
+++ web-scraper/server/main.js
@@ -1,5 +1,39 @@
 const path = require('path')
+const express = require('express')
+
+const app = express()
+const cors = require('cors')
+app.use(cors({origin: true}))
+
+const PORT = process.env.PORT || 5000
 
 const withReact = true //set to false if you don't want react
+const url = withReact ? path.resolve(__dirname, '../react-challenge/build', 'index.html') : path.resolve(__dirname, '../vanilla-challenge', 'index.html')
 
-const url = withReact ? '' : ''
+
+
+//Answer API requests ---
+app.get('/api/createSheet', (req, res)=>{
+	res.set('Content-Type', 'application/json');
+	res.send('{"message":"Hello from the custom server!"}')
+})
+
+
+
+app.get('/api/getTextSnippets', (req, res) => {
+	res.json('Hello world')
+})
+
+
+
+//Send all requests (other than  api requests) to correct index.html
+app.get('*', (req, res)=>{
+	res.sendFile(url)
+})
+
+
+
+//Start listening for requests
+app.listen(PORT, (e)=>{
+	console.error(e)
+})
