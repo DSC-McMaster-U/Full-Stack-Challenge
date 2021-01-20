7710db6f720595785184dde6e1fc199883166096
Youssof Fahmy <youssoffahmy@gmail.com>
2021-01-20 17:29:37 -0500

    fixed error with serving react files

--- web-scraper/react-challenge/src/App.js
+++ web-scraper/react-challenge/src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import {useState} from 'react'
+import React, {useState} from 'react'
 import {Card, Container, FormControl, Button} from 'react-bootstrap'
 
 function App() {
--- web-scraper/server/main.js
+++ web-scraper/server/main.js
@@ -1,14 +1,15 @@
 const path = require('path')
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
-const cors = require('cors')
-app.use(cors({origin: true}))
-
-const PORT = process.env.PORT || 5000
 
 const withReact = true //set to false if you don't want react
-const url = withReact ? path.resolve(__dirname, '../react-challenge/build', 'index.html') : path.resolve(__dirname, '../vanilla-challenge', 'index.html')
+const url = withReact ? path.join(__dirname, '../react-challenge/build') : path.resolve(__dirname, '../vanilla-challenge')
+const PORT = process.env.PORT || 5000
+
+app.use(cors({origin: true}))
+app.use(express.static(url))
 
 
 
@@ -25,15 +26,5 @@ app.get('/api/getTextSnippets', (req, res) => {
 })
 
 
-
-//Send all requests (other than  api requests) to correct index.html
-app.get('*', (req, res)=>{
-	res.sendFile(url)
-})
-
-
-
 //Start listening for requests
-app.listen(PORT, (e)=>{
-	console.error(e)
-})
+app.listen(PORT)
