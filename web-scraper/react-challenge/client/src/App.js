5d589e4b5cc02cc6b9922ceb43c50af30d61ae1b
Youssof Fahmy <youssoffahmy@gmail.com>
2021-01-30 16:41:29 -0500

    uploading file

--- web-scraper/react-challenge/client/src/App.js
+++ web-scraper/react-challenge/client/src/App.js
@@ -1,13 +1,26 @@
 import './App.css'
+import axios from 'axios'
 import React, {useState} from 'react'
 import {Card, Container, FormControl, Button} from 'react-bootstrap'
 
 function App() {
 	
-	const [url, setUrl] = useState('')
+	const [url, setUrl] = useState('http://google.com')
 	
-	function submit(){
-		console.log(url)
+	const [screenshotURL, setScreenshotURl] = useState('')
+	
+	async function submit(){
+		try{
+			const result = await axios.post('http://localhost:5000/api/screenshot', {
+				url: url
+			})
+
+			console.log(result)
+			setScreenshotURl(result.data.url)
+		}catch(err){
+			console.error(err)
+		}
+		
 	}
 	
 	return (
@@ -22,14 +35,13 @@ function App() {
 						value={url} 
 						onChange={e=>setUrl(e.target.value)}
 					/>
-					
 					<Button  className = 'submitButton' onClick={submit}>Submit</Button>
 				</div>
 				
 				
 				<Card className='mainCard'>
 					<Card.Body>
-						Scraped Content Goes Here!
+						<img src={screenshotURL} style={{width:'100%', height:'100%'}}/>
 					</Card.Body>
 					
 				</Card>
--- web-scraper/react-challenge/server.js
+++ web-scraper/react-challenge/server.js
@@ -4,6 +4,11 @@ const cors = require('cors')
 const puppeteer = require('puppeteer')
 const { v4: uuidv4 } = require('uuid')
 const bodyParser = require('body-parser')
+const { BlobServiceClient } = require('@azure/storage-blob');
+
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
 
 const app = express()
 app.use(cors({origin: true}))
@@ -13,33 +18,40 @@ app.use(express.static(path.join(__dirname, './client/build')))
 
 
 //Take a screenshot of a given page
-app.post('/api/screenshot/:d', async (req, res)=>{
+app.post('/api/screenshot', async (req, res)=>{
+	
+	//Check if url is blank
+	if (!req.body.url){
+		res.status(500).json({error:'Missing URL'})
+	}
+	
 	try {
+		//take the screenshot as base64
 		const browser = await puppeteer.launch({ headless: true })
 		const page = await browser.newPage()
-		const saveTo = __dirname + '/screenshots/' + uuidv4() + '.jpg'
-		
-		console.log(req.body)
-		
 		await page.goto(req.body.url)
+		const screenshot = await page.screenshot({type: "jpeg", encoding: 'binary'})
+		const blobName = uuidv4() + '.jpg'
 		
-		await page.screenshot({
-			path: saveTo,
-			type: "jpeg",
-		})
 		
+		//Upload to azure
+		const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING)
+		const containerClient = blobServiceClient.getContainerClient(process.env.AZURE_STORAGE_CONTAINER)
+		const blockBlobClient = containerClient.getBlockBlobClient(blobName)
+		const uploadBlobResponse = await blockBlobClient.upload(screenshot, screenshot.length);
+		
+		//Close puppeteer
 		await page.close()
 		await browser.close()
 		
-		return res.json({url: saveTo})
+		return res.json({url:'Works'})
 		
 	} catch (err) {
 		console.error(err)
-		res.status(500).json({error:'Unable to create screenshot!'})
+		res.status(500).json(JSON.stringify(err))
 	}
 })
 
 //Start listening for requests
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
-
