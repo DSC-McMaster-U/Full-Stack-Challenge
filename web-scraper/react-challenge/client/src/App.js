5bb490e35d0386b5bf99cb89e282bebf6584f46f
Youssof Fahmy <youssoffahmy@gmail.com>
2021-01-31 15:19:52 -0500

    removed solution code fromm boilerplaye

--- web-scraper/react-challenge/client/src/App.js
+++ web-scraper/react-challenge/client/src/App.js
@@ -1,30 +1,8 @@
 import './App.css'
-import axios from 'axios'
-import React, {useState} from 'react'
+import React from 'react'
 import {Container, FormControl, Button} from 'react-bootstrap'
-import ImageCard from './ImageCard'
 
 export default function App() {
-	
-	const [url, setUrl] = useState('http://google.com')
-	const [loading, setLoading] = useState(false)
-	
-	const [screenshotURL, setScreenshotURl] = useState('')
-	
-	async function submit(){
-		setLoading(true)
-		try{
-			const result = await axios.post('http://localhost:5000/api/screenshot', {
-				url: url
-			})
-			
-			console.log(result)
-			setScreenshotURl(result.data.url)
-		}catch(err){
-			console.error(err)
-		}
-		setLoading(false)
-	}
 	
 	return (
 		<div className="App">
@@ -35,21 +13,15 @@ export default function App() {
 					<FormControl 
 						className='urlBar' 
 						placeholder='Enter a URL'
-						value={url} 
-						onChange={e=>setUrl(e.target.value)}
 					/>
 					
 					<div className="submitHolder flexCenter">
-						<Button className='submitButton' onClick={submit}>Submit</Button>
+						<Button className='submitButton'>Submit</Button>
 					</div>
 				</div>
-				
-				<ImageCard url = {screenshotURL} loading={loading}/>
 					
 			</Container>
 			
 		</div>
 	)
 }
-
-
